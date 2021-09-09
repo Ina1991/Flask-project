bc5663c06cec592128152666b1aa86232bf5b905
Ina1991 <zou.yiwei@silu.asia>
2021-09-09 13:31:25 +0800

    COMMIT

---
+++ SQLQuery9.sql
@@ -0,0 +1,12 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [id]
+      ,[slaveId]
+      ,[datStart]
+      ,[datEnd]
+      ,[duration]
+      ,[channel1]
+      ,[channel2]
+      ,[channel3]
+      ,[channel4]
+      ,[error]
+  FROM [WERMAWIN].[dbo].[slaveData]
