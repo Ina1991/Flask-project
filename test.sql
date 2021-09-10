535fae4560d315e2442ebe8a0435e6145d82e9fb
Ina1991 <zou.yiwei@silu.asia>
2021-09-10 19:56:46 +0800

    commit

---
+++ test.sql
@@ -0,0 +1,12 @@
+SELECT TOP (1000) [id]
+      ,[slaveId]
+      ,[datStart]
+      ,[datEnd]
+      ,[duration]
+      ,[channel1]
+      ,[channel2]
+      ,[channel3]
+      ,[channel4]
+      ,[error]
+  FROM [WERMAWIN].[dbo].[slaveData]
+
